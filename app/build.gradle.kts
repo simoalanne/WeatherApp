0a62cd582dd56405d933c15ccd49e62b1b4a93e5
Simo <simo.alanne@tuni.fi>
2025-05-23 20:45:08 +0300

    Increment app version codes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         // the Geocoder API this app uses no longer needs fallback code for older versions.
         minSdk = 33
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
